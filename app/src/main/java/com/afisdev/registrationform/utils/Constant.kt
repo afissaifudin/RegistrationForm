2518b32227fd689a3d87638f1f95d49fd4309ee7
Afis Legion PC <afis.saifudin@stockbit.com>
2023-09-11 01:01:08 +0700

    add handler for input dropdown in customEditText

--- app/src/main/java/com/afisdev/registrationform/utils/Constant.kt
+++
@@ -1,6 +0,0 @@
-package com.afisdev.registrationform.utils
-
-object Constant {
-
-    const val api_key = "api_key"
-}
---
+++ common/src/main/java/com/afisdev/common/util/Constant.kt
@@ -0,0 +1,7 @@
+package com.afisdev.common.util
+
+object Constant {
+
+    const val api_key = "api_key"
+    const val date_format = "dd-MM-yyyy"
+}
