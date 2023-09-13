ca852e4e8b5bb3ab46cad8671318ce440d204ac8
Afis Legion PC <afis.saifudin@stockbit.com>
2023-09-14 02:04:09 +0700

    update logic ui on residential data fragment

---
+++ app/src/main/java/com/afisdev/registrationform/feature/residentialdata/ResidentialDataEntity.kt
@@ -0,0 +1,12 @@
+package com.afisdev.registrationform.feature.residentialdata
+
+
+/**
+ * Created by afisdev on 13/09/2023.
+ */
+data class ResidentialDataEntity(
+    val domicile: String?,
+    val housingType: String?,
+    val housingNumber: String?,
+    val province: String?
+)
---
+++ app/src/main/java/com/afisdev/registrationform/utils/HousingType.kt
@@ -0,0 +1,10 @@
+package com.afisdev.registrationform.utils
+
+
+/**
+ * Created by afisdev on 11/09/2023.
+ */
+enum class HousingType {
+    Home,
+    Office
+}
